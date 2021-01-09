82dd60ccd7d93dc463bf94d6515309eda65d03ce
f0r3st3r <77185962+f0r3st3r@users.noreply.github.com>
2021-01-08 22:32:25 -0800

    Update soup_pg.py

--- soup_pg.py
+++ soup_pg.py
@@ -3,5 +3,7 @@ from bs4 import BeautifulSoup
 import sys
 import requests
 
-page = requests.get(sys.argv[1])
+headers = { "User-Agent":"Mozilla 5.0" }
+
+page = requests.get(sys.argv[1], headers=headers)
 soup = BeautifulSoup(page.text, "lxml")
